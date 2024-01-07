e236c10b1ad93dbede10cf19cf2c697c83898541
Henry Deutsch <henrymdeutsch@gmail.com>
2024-01-07 11:32:46 -0500

    committing current website

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,68 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import TestimonialSwiper from './components/TestimonialSwiper.jsx'
+import { toggle, smoothScroll } from './utils/general.js';
+
 import './App.css'
+import 'swiper/css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+    {/* start navigation bar */}
+    <div className="topnav" id="myTopnav">
+      <a href="#home" className="name">Henry Magnus Deutsch</a>
+      <div className="horizontal-tabs">
+        <a href="#experience">Experience</a>
+        <a href="#skills">Skills</a>
+        <a href="#testimonials" onClick={smoothScroll}>Testimonials</a>
+        <a href="#featured">Featured Projects</a>
+      </div>
+
+      <a href="javascript:void(0);" className="icon" onClick={toggle}>
+        <i className="fa fa-bars"></i>
+      </a>
+    </div>
+
+    {/* start content */}
+    <div className="content">
+      {/* start above the fold section */}
+      <div className="header">
+        <div className="header-text">
+          <p className="subhead-small">Software Developer</p>
+          <p className="subhead-big">
+            Computer science major at Johns Hopkins University
+          </p>
+          <div>
+            <p className="subhead-rainbow">Hi, I'm Henry</p>
+          </div>
+          <a className="subhead-contact" href="#contact">Schedule a meeting</a>
+          <img
+            className="background-image avatar"
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt="Avatar"
+          />
+        </div>
+        <div className="header-image">
+          <img
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt="Avatar"
+            className="avatar"
+          />
+        </div>
+      </div>
+
+      {/* start testimonials section */}
+      <div className="testimonials" id="testimonials">
+        <h2>Testiminials</h2>
+
+        <TestimonialSwiper />
+      </div>
+
+      {/* start featured projects section */}
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Featured Projects</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
   )
 }
